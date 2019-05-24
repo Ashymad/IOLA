5bbe5c1e7289316b9ced20f2975a1979cda67cba
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-05-24 20:28:33 +0200

    Dont mod, add more data

--- test.jl
+++ test.jl
@@ -5,51 +5,53 @@ using WAV
 using Gnuplot
 
 #period = 36.0 #mp3
-period = 68.0 #aac
+#period = 68.0 #aac
+#period = 17 #ac3?
 
 codec = "ogg"
 
 params = Codec.getParams(Codec.OGG)
-hop = params.hop
+seg_hop = params.hop
 transform = Codec.getTransformFunction(params)
 
+bitrates = [128, 196, 256, 320]
+
+diffs = Vector{Array{Float64, 2}}()
+
 (y, fs, nbits, opt) = wavread("data/audio.wav")
+seg_len = round(Int, fs/params.length)*params.length
 
-diffs = analyze(y[1:div(end,2),1], transform, convert(Int, fs), hop)
+push!(diffs, analyze(y[:,1], transform, seg_len, seg_hop))
 
-(y, fs, nbits, opt) = wavread("data/$codec/audio-128.wav")
 
-diffs128 = analyze(y[1:div(end,2),1], transform, convert(Int, fs), hop)
-
-(y, fs, nbits, opt) = wavread("data/$codec/audio-196.wav")
-
-diffs196 = analyze(y[1:div(end,2),1], transform, convert(Int, fs), hop)
-
-(y, fs, nbits, opt) = wavread("data/$codec/audio-256.wav")
-
-diffs256 = analyze(y[1:div(end,2),1], transform, convert(Int, fs), hop)
-
-(y, fs, nbits, opt) = wavread("data/$codec/audio-320.wav")
-
-diffs320 = analyze(y[1:div(end,2),1], transform, convert(Int, fs), hop)
+for bitr in bitrates
+    (y, fs, nbits, opt) = wavread("data/$codec/audio-$bitr.wav")
+    push!(diffs, analyze(y[:,1], transform, seg_len, seg_hop))
+end
 
 @gp(:GP1, "set angles radians",
     "set polar",
     "set grid polar 15. lt -1 dt 0 lw 0.5",
     "unset border", "unset param",
     "unset xtics",
-    2π*mod.(diffs128[:,2], period)/period, diffs128[:,1], "pt 7 ps 2 t '128'",
-    2π*mod.(diffs196[:,2], period)/period, diffs196[:,1], "pt 7 ps 2 t '196'",
-    2π*mod.(diffs256[:,2], period)/period, diffs256[:,1], "pt 7 ps 2 t '256'",
-    2π*mod.(diffs320[:,2], period)/period, diffs320[:,1], "pt 7 ps 2 t '320'",
-    2π*mod.(diffs[:,2], period)/period, diffs[:,1], "pt 7 ps 2 t 'none'"
+    2π*diffs[2][:,2]/seg_hop, diffs[2][:,1], "pt 7 ps 2 t '128k'",
+    2π*diffs[3][:,2]/seg_hop, diffs[3][:,1], "pt 7 ps 2 t '196k'",
+    2π*diffs[4][:,2]/seg_hop, diffs[4][:,1], "pt 7 ps 2 t '256k'",
+    2π*diffs[5][:,2]/seg_hop, diffs[5][:,1], "pt 7 ps 2 t '320k'",
+    2π*diffs[1][:,2]/seg_hop, diffs[1][:,1], "pt 7 ps 2 t 'none'"
    )
 
-xs = 1:hop/2
-periodf = zeros(length(xs))
-for i = eachindex(periodf)
-    per = mod.(diffs128[:,2],xs[i])
-    periodf[i] = sum(abs.(per .- sum(per)/length(per)))
+xs = 1:seg_hop
+periodf = zeros(length(xs), 5)
+for i = 1:length(xs), j = 1:5
+    per = mod.(diffs[j][:,2],xs[i])
+    periodf[i, j] = sum(abs.(per .- sum(per)/length(per)))
 end
 
-@gp :GP2 xs periodf "w l tit 'Period'"
+@gp(:GP2,
+    xs, periodf[:,2], "w l tit '128k'",
+    xs, periodf[:,3], "w l tit '196k'",
+    xs, periodf[:,4], "w l tit '256k'",
+    xs, periodf[:,5], "w l tit '320k'",
+    xs, periodf[:,1], "w l tit 'none'"
+   )
