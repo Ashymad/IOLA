ca244ce48006bc8ca4aa13fad74d2cd97f52b89e
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-05-28 00:10:22 +0200

    Find period automagically

--- test.jl
+++ test.jl
@@ -3,14 +3,15 @@ using MDCT
 using DSP
 using WAV
 using Gnuplot
+using Statistics
 
 #period = 36.0 #mp3
 #period = 68.0 #aac
 #period = 17 #ac3?
 
-codec = "aac"
+codec = "wma"
 
-params = Codec.getParams(Codec.AAC)
+params = Codec.getParams(Codec.WMA)
 seg_hop = params.hop
 transform = Codec.getTransformFunction(params)
 
@@ -23,30 +24,16 @@ seg_len = round(Int, fs/params.length)*params.length
 
 push!(diffs, analyze(y[:,1], transform, seg_len, seg_hop))
 
-
 for bitr in bitrates
     (y, fs, nbits, opt) = wavread("data/$codec/audio-$bitr.wav")
     push!(diffs, analyze(y[:,1], transform, seg_len, seg_hop))
 end
 
-@gp(:GP1, "set angles radians",
-    "set polar",
-    "set grid polar 15. lt -1 dt 0 lw 0.5",
-    "unset border", "unset param",
-    "unset xtics",
-    2π*diffs[2][:,2]/seg_hop, diffs[2][:,1], "pt 7 ps 2 t '128k'",
-    2π*diffs[3][:,2]/seg_hop, diffs[3][:,1], "pt 7 ps 2 t '196k'",
-    2π*diffs[4][:,2]/seg_hop, diffs[4][:,1], "pt 7 ps 2 t '256k'",
-    2π*diffs[5][:,2]/seg_hop, diffs[5][:,1], "pt 7 ps 2 t '320k'",
-    2π*diffs[1][:,2]/seg_hop, diffs[1][:,1], "pt 7 ps 2 t 'none'"
-   )
-
-xs = 1:seg_hop
+xs = 3:seg_hop
 periodf = zeros(length(xs), 5)
 for i = 1:length(xs), j = 1:5
     per = mod.(diffs[j][:,2],xs[i])
-    sump = sum(per)
-    periodf[i, j] = sum(abs.(per .- sump/length(per)))/sump
+    periodf[i, j] = std(per; corrected=false)/mean(per)
 end
 
 @gp(:GP2,
@@ -56,3 +43,18 @@ end
     xs, periodf[:,5], "w l tit '320k'",
     xs, periodf[:,1], "w l tit 'none'"
    )
+
+period = xs[end+1-findmin(periodf[end:-1:1,2])[2]]
+
+@gp(:GP1, "set angles radians",
+    "set polar",
+    "set grid polar 15. lt -1 dt 0 lw 0.5",
+    "unset border", "unset param",
+    "unset xtics",
+    2π*mod.(diffs[2][:,2], period)/period, diffs[2][:,1], "pt 7 ps 2 t '128k'",
+    2π*mod.(diffs[3][:,2], period)/period, diffs[3][:,1], "pt 7 ps 2 t '196k'",
+    2π*mod.(diffs[4][:,2], period)/period, diffs[4][:,1], "pt 7 ps 2 t '256k'",
+    2π*mod.(diffs[5][:,2], period)/period, diffs[5][:,1], "pt 7 ps 2 t '320k'",
+    2π*mod.(diffs[1][:,2], period)/period, diffs[1][:,1], "pt 7 ps 2 t 'none'"
+   )
+
