a4511e8f22c2b72a8cd346ce96f12e65aced9ca8
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-06-13 09:45:59 +0200

    Train on most of the data.

--- train_knet.jl
+++ train_knet.jl
@@ -31,8 +31,8 @@ end
 
 minibatch_size = 4
 batch_size = 16
-train_size = 512
-test_size = 256
+train_size = 4096
+test_size = 2048
 
 train_batch_idxs = partition(1:train_size, batch_size)
 test_batch_idxs = partition((train_size+1):(train_size+test_size), batch_size)
@@ -57,27 +57,36 @@ model = Chain(Conv(3, 3, 1, 16),
               Dense(512, 256, pdrop=0.2),
               Dense(256, 4, identity, pdrop=0.2))
 
+test_accs = Array{Float32, 1}()
+
 h5open("./dataset.h5") do file
     data = file["data"]
     labels = file["labels"]
 
-    for idx in progress(train_batch_idxs)
-        let dtrn = make_batch(data, labels, idx, minibatch_size)
-            for a in adam(model, dtrn); end
-        end
-        print(" asdas")
-        gc()
-    end
+    for epoch in 1:10
+        @info "Epoch: $epoch"
 
-    acc = 0
-    for idx in test_batch_idxs
-        let dtst = make_batch(data, labels, idx, minibatch_size)
-            acc += accuracy(model, dtst)
+        for idx in progress(train_batch_idxs)
+            let dtrn = make_batch(data, labels, idx, minibatch_size)
+                for a in adam(model, dtrn); end
+            end
+            gc()
         end
-        gc()
+
+        test_acc = 0
+        for idx in progress(test_batch_idxs)
+            let dtst = make_batch(data, labels, idx, minibatch_size)
+                test_acc += accuracy(model, dtst)
+            end
+            gc()
+        end
+        test_acc /= length(test_batch_idxs)
+
+        push!(test_accs, test_acc)
+
+        @info "Test Accuracy: $test_acc"
+
     end
-    acc /= length(test_batch_idxs)
-    @info "Test Accuracy: $acc"
 
 end
 
