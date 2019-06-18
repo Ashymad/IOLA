cf3e243fd48858642764a4b7d8c88e04529c71fd
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-06-18 18:17:21 +0200

    Cleanup functionality; shuffle data.

--- train_knet.jl
+++ train_knet.jl
@@ -6,6 +6,7 @@ using DSP.Windows: hanning
 using HDF5
 using Base.Iterators: flatten, cycle, take, partition
 using Statistics: mean, std
+using Random: shuffle!
 using Knet
 using Knet: Data
 
@@ -20,22 +21,24 @@ function make_batch(data, labels, idxs, minibatch_size; n = 512, noverlap = div(
     nfft = nextfastfft(n)
     sz = div(size(data, 1), noverlap) - 1
     X_batch = Array{Float32}(undef, div(nfft, 2) + 1, sz, 1, length(idxs))
+    Y_batch = Array{Int8}(undef, length(idxs))
     win = hanning(nfft)
     for i in 1:length(idxs)
         st = abs.(stft(Float32.(data[:,idxs[i]][:]./typemax(Int16)), n, noverlap, window=win, nfft=nfft))
         X_batch[:, :, 1, i] = epsnormalise(20*log10.(max.(st, eps(Float32))))
+        Y_batch[i] = labels[idxs[i]][1] + 1
     end
-    Y_batch = labels[idxs[1]:idxs[end]] .+ 1
-    return minibatch(X_batch, Y_batch, ceil(Int64, length(idxs) / minibatch_size))
+    return minibatch(X_batch, Y_batch, minibatch_size)
 end
 
 minibatch_size = 4
-batch_size = 16
-train_size = 4096
-test_size = 2048
+train_batch_size = 16
+test_batch_size = 64
+train_size = 512
+test_size = 512
 
-train_batch_idxs = partition(1:train_size, batch_size)
-test_batch_idxs = partition((train_size+1):(train_size+test_size), batch_size)
+train_batch_idxs = collect(partition(1:train_size, train_batch_size))
+test_batch_idxs = partition((train_size+1):(train_size+test_size), test_batch_size)
 
 struct Conv; w; b; f; p; end
 (c::Conv)(x) = c.f.(pool(conv4(c.w, dropout(x,c.p)) .+ c.b))
@@ -50,44 +53,56 @@ struct Chain; layers; Chain(layers...) = new(layers); end
 (c::Chain)(x,y) = nll(c(x),y)
 (c::Chain)(d::Data) = mean(c(x,y) for (x,y) in d)
 
-model = Chain(Conv(3, 3, 1, 16),
+function train!(epochs, load=true)
+
+    model, test_accs = if load
+        lf = Knet.load("model2.jld2")
+        lf["model"], lf["test_accs"]
+    else
+        Chain(Conv(3, 3, 1, 16),
               Conv(3, 3, 16, 16),
               Conv(3, 3, 16, 16),
-              Dense(309120, 512, pdrop=0.2),
-              Dense(512, 256, pdrop=0.2),
-              Dense(256, 4, identity, pdrop=0.2))
-
-test_accs = Array{Float32, 1}()
-
-h5open("./dataset.h5") do file
-    data = file["data"]
-    labels = file["labels"]
-
-    for epoch in 1:10
-        @info "Epoch: $epoch"
-
-        for idx in progress(train_batch_idxs)
-            let dtrn = make_batch(data, labels, idx, minibatch_size)
-                for a in adam(model, dtrn); end
-            end
-            gc()
-        end
-
-        test_acc = 0
-        for idx in progress(test_batch_idxs)
-            let dtst = make_batch(data, labels, idx, minibatch_size)
-                test_acc += accuracy(model, dtst)
-            end
-            gc()
-        end
-        test_acc /= length(test_batch_idxs)
-
-        push!(test_accs, test_acc)
-
-        @info "Test Accuracy: $test_acc"
-
+              Dense(309120, 256, pdrop=0.3),
+              Dense(256, 256, pdrop=0.2),
+              Dense(256, 4, identity, pdrop=0.2)),
+        Array{Array{Float32, 1}, 1}()
     end
 
-end
+    h5open("./dataset.h5") do file
+        data = file["data"]
+        labels = file["labels"]
 
+        for epoch in 1:epochs
+            @info "Epoch: $epoch"
+            shuffle!(train_batch_idxs)
+
+            for idx in progress(train_batch_idxs)
+                let dtrn = make_batch(data, labels, idx, minibatch_size)
+                    for a in adam(model, dtrn); end
+                end
+                gc()
+            end
+
+            test_acc = zeros(4)
+            for idx in progress(test_batch_idxs)
+                for divo in 0:3
+                    let dtst = make_batch(data, labels, filter((x) -> mod(x,4) == divo, idx), minibatch_size)
+                        test_acc[divo+1] += accuracy(model, dtst)
+                    end
+                end
+                gc()
+            end
+            test_acc /= length(test_batch_idxs)
+
+            push!(test_accs, test_acc)
+
+            @info "Test Accuracies: $test_acc, Mean: $(mean(test_acc))"
+            if length(test_accs) > 1 && mean(test_acc) > mean(maximum(test_accs[1:(end-1)]))
+                Knet.save("model2.jld2", "model", model, "test_accs", test_accs)
+            end
+        end
+    end
+
+    return model, test_accs
+end
 
