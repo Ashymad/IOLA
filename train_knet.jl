4861b3cadccac3509e44e7a939d6e64d837e8f12
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-06-19 16:37:49 +0200

    Show confusion matrix; reduce amount of data

--- train_knet.jl
+++ train_knet.jl
@@ -9,6 +9,7 @@ using Statistics: mean, std
 using Random: shuffle!
 using Knet
 using Knet: Data
+using LinearAlgebra: diag
 
 function epsnormalise(x::AbstractArray{T}; dims=1, ϵ=eps(T)) where T
     μ = mean(x, dims = dims)
@@ -17,14 +18,15 @@ function epsnormalise(x::AbstractArray{T}; dims=1, ϵ=eps(T)) where T
     return (x .- μ) ./ σ
 end
 
-function make_batch(data, labels, idxs, minibatch_size; n = 512, noverlap = div(n, 2))
+function make_batch(data, labels, idxs, minibatch_size; divider = 1, n = 512, noverlap = div(n, 2))
     nfft = nextfastfft(n)
-    sz = div(size(data, 1), noverlap) - 1
+    sz = div(div(size(data, 1), noverlap), divider) - 1
     X_batch = Array{Float32}(undef, div(nfft, 2) + 1, sz, 1, length(idxs))
     Y_batch = Array{Int8}(undef, length(idxs))
     win = hanning(nfft)
+    data_ind = 1:div(size(data, 1), divider)
     for i in 1:length(idxs)
-        st = abs.(stft(Float32.(data[:,idxs[i]][:]./typemax(Int16)), n, noverlap, window=win, nfft=nfft))
+        st = abs.(stft(Float32.(data[data_ind,idxs[i]][:]./typemax(Int16)), n, noverlap, window=win, nfft=nfft))
         X_batch[:, :, 1, i] = epsnormalise(20*log10.(max.(st, eps(Float32))))
         Y_batch[i] = labels[idxs[i]][1] + 1
     end
@@ -34,18 +36,16 @@ end
 function confusion(model, data, size)
     conf = zeros(Float32, size, size)
     for mbatch in data
-        confl = zeros(Float32, size, size)
         pred = model(mbatch[1])
         for ind = 1:length(mbatch[2])
-            confl[findmax(pred[ind, :])[2], mbatch[2][ind]] += 1
+            conf[findmax(pred[ind, :])[2], mbatch[2][ind]] += 1
         end
-        conf += confl ./ length(mbatch[2])
     end
-    return conf ./ maximum(conf)
+    return conf ./ length(data)
 end
 
 minibatch_size = 4
-train_batch_size = 16
+train_batch_size = 32
 test_batch_size = 64
 train_size = 512
 test_size = 512
@@ -68,18 +68,19 @@ struct Chain; layers; Chain(layers...) = new(layers); end
 
 function train!(epochs, load=true)
 
-    model, test_accs = if load
+    model, test_confs = if load
         lf = Knet.load("model2.jld2")
-        lf["model"], lf["test_accs"]
+        lf["model"], lf["test_confs"]
     else
         Chain(Conv(3, 3, 1, 16),
               Conv(3, 3, 16, 16),
               Conv(3, 3, 16, 16),
-              Dense(309120, 256, pdrop=0.3),
+              Dense(50400, 256, pdrop=0.3),
               Dense(256, 256, pdrop=0.2),
               Dense(256, 4, identity, pdrop=0.2)),
-        Array{Array{Float32, 1}, 1}()
+        Array{Array{Float32, 2}, 1}()
     end
+    best_acc = length(test_confs) > 0 ? mean(diag(test_confs[end])) : 0
 
     h5open("./dataset.h5") do file
         data = file["data"]
@@ -90,32 +91,35 @@ function train!(epochs, load=true)
             shuffle!(train_batch_idxs)
 
             for idx in progress(train_batch_idxs)
-                let dtrn = make_batch(data, labels, idx, minibatch_size)
+                let dtrn = make_batch(data, labels, idx, minibatch_size, divider=6)
                     for a in adam(model, dtrn); end
                 end
                 gc()
             end
 
-            test_acc = zeros(4)
+            test_conf = zeros(4, 4)
             for idx in progress(test_batch_idxs)
-                for divo in 0:3
-                    let dtst = make_batch(data, labels, filter((x) -> mod(x,4) == divo, idx), minibatch_size)
-                        test_acc[divo+1] += accuracy(model, dtst)
-                    end
+                let dtst = make_batch(data, labels, idx, minibatch_size, divider=6)
+                    test_conf .+= confusion(model, dtst, 4)
                 end
                 gc()
             end
-            test_acc /= length(test_batch_idxs)
+            test_conf ./= length(test_batch_idxs)
 
-            push!(test_accs, test_acc)
+            push!(test_confs, test_conf)
 
-            @info "Test Accuracies: $test_acc, Mean: $(mean(test_acc))"
-            if length(test_accs) > 1 && mean(test_acc) > maximum(mean.(test_accs[1:(end-1)]))
-                Knet.save("model2.jld2", "model", model, "test_accs", test_accs)
+            acc = mean(diag(test_conf))
+
+            println("Confusion Matrix:")
+            display(test_conf)
+            println("Accuracy: $acc")
+            if acc > best_acc
+                best_acc = acc
+                Knet.save("model2.jld2", "model", model, "test_confs", test_confs)
             end
         end
     end
 
-    return model, test_accs
+    return model, test_confs
 end
 
