6d8de048b947dd62014048688e4dc3f123256f19
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-07-10 00:03:09 +0200

    Add possiibility for multiple codecs

--- construct_dataset.py
+++ construct_dataset.py
@@ -3,23 +3,30 @@ from pydub.effects import normalize
 import os
 import numpy as np
 from os.path import join, splitext
-from random import randint
+from random import randint, shuffle
 import h5py as h5
 
-fragment_length = 30
+fragment_length = 10
 maxsize = 0
 fs = 44100
 bd = 16
 
-fmt = "mp3"
-bitrates = [None, "320k", "192k", "128k"]
-bitrates_labels = [0, 1, 2, 3]
-lenbr = len(bitrates)
+fmt = [None, "mp3", "aac", "ogg"]
+ffmpg_filt = [None, "mp3", "adts", "ogg"]
+bitrates = ["320k", "192k", "128k"]
+bitrates_labels = [1, 2, 3]
+fmt_labels = [0, 10, 20, 30]
+lenbr = len(bitrates)*len(fmt) - len(bitrates) + 1
+
+allfiles = []
 
 for (root, dirs, files) in os.walk(os.getcwd()):
     for cfile in files:
         if splitext(cfile)[-1].lower() == ".flac":
-            maxsize += lenbr
+            allfiles.append(join(root, cfile))
+
+maxsize = len(allfiles)*lenbr
+shuffle(allfiles)
 actualsize = 0
 
 with h5.File('dataset.h5', 'w') as f:
@@ -28,30 +35,31 @@ with h5.File('dataset.h5', 'w') as f:
     data = f.create_dataset("data", (maxsize, fragment_length*fs),
                             maxshape=(maxsize, fragment_length*fs),
                             dtype=f"int{bd}")
-    for (root, dirs, files) in os.walk(os.getcwd()):
+    for fpath in allfiles:
         try:
-            for cfile in files:
-                if splitext(cfile)[-1].lower() != ".flac":
-                    continue
-                fpath = join(root, cfile)
+            audio = AudioSegment\
+                .from_file(fpath, format="flac")
+            if audio.duration_seconds > fragment_length and \
+               audio.frame_rate == fs:
                 print(f"Preparing: {fpath} ({int(actualsize/lenbr)}/{int(maxsize/lenbr)})")
-                audio = AudioSegment\
-                    .from_file(fpath, format="flac")
-                if audio.duration_seconds > fragment_length and \
-                   audio.frame_rate == fs:
-                    audio = normalize(audio.set_channels(1).set_sample_width(bd//8))
-                    start = randint(0, np.floor((audio.duration_seconds-fragment_length)*1000))
-                    fragment = audio[start:(fragment_length*1000+start)]
-                    for i in range(lenbr):
-                        ind = i + actualsize
-                        labels[ind] = bitrates_labels[i]
-                        if bitrates[i] is not None:
-                            fragment.export(f"/tmp/tmp.{fmt}", format=fmt,
-                                            bitrate=bitrates[i])
-                            fragment = AudioSegment.from_file(f"/tmp/tmp.{fmt}",
-                                                              format=fmt)
-                        data[ind, :] = fragment.get_array_of_samples()
-                    actualsize += lenbr
+                audio = normalize(audio.set_sample_width(bd//8))
+                start = randint(0, np.floor((audio.duration_seconds-fragment_length)*1000))
+                fragment = audio[start:(fragment_length*1000+start)]
+                for i1 in range(len(fmt)):
+                    if fmt[i1] is None:
+                        labels[actualsize] = fmt_labels[i1]
+                        data[actualsize, :] = fragment.set_channels(1).get_array_of_samples()
+                        actualsize += 1
+                    else:
+                        for i2 in range(len(bitrates)):
+                            labels[actualsize] = bitrates_labels[i2] + fmt_labels[i1]
+                            fragment.export(f"/tmp/tmp.{fmt[i1]}",
+                                            format=ffmpg_filt[i1],
+                                            bitrate=bitrates[i2])
+                            fragment = AudioSegment.from_file(f"/tmp/tmp.{fmt[i1]}",
+                                                              format=fmt[i1])
+                            data[actualsize, :] = fragment.set_channels(1).get_array_of_samples()[0:fragment_length*fs]
+                            actualsize += 1
         except KeyboardInterrupt:
             print("Received SIGINT, exiting...")
             break
