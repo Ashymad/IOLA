209f08b3be3a300382e38cec2e3e12c09bde41d8
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-07-19 15:18:55 +0200

    Remove old trains; make it work on gpu and for all codecs

--- train.jl
+++ train.jl
@@ -1,26 +1,14 @@
-using CuArrays
-using Flux
-using Flux: onecold, onehotbatch, ADAM
-using Base.Iterators: partition
-using Statistics
-using DSP.Periodograms
-import DSP.Periodograms.stft
-using DSP.Windows
-using HDF5
-using Printf, BSON
+ENV["COLUMNS"]=72
 import Base.GC.gc
-
-function stft(s::AbstractArray{T, 4},n::Int=length(s)>>3, noverlap::Int=n>>1,
-              psdonly::Union{Nothing,Periodograms.PSDOnly}=nothing;
-              onesided::Bool=eltype(s)<:Real, nfft::Int=nextfastfft(n), fs::Real=1,
-              window::Union{Function,AbstractVector,Nothing}=nothing) where T
-    sz = div(size(s, 1), noverlap) - 1
-    out = zeros(Complex{T}, div(nfft, 2) + 1, sz, 1, size(s, 4))
-    for i = 1:size(s, 4)
-        @views out[:,:,1,i] = stft(s[:,1,1,i][:], n, noverlap, psdonly; onesided=onesided, nfft=nfft, fs=fs, window=window)
-    end
-    out
-end
+using DSP.Periodograms: stft, nextfastfft
+using DSP.Windows: hanning
+using HDF5
+using Base.Iterators: flatten, cycle, take, partition
+using Statistics: mean, std
+using Random: shuffle!
+using Knet
+using Knet: Data, KnetArray
+using LinearAlgebra: diag
 
 function epsnormalise(x::AbstractArray{T}; dims=1, ϵ=eps(T)) where T
     μ = mean(x, dims = dims)
@@ -29,156 +17,131 @@ function epsnormalise(x::AbstractArray{T}; dims=1, ϵ=eps(T)) where T
     return (x .- μ) ./ σ
 end
 
-function loss(x, y, print_loss=true)
-    ŷ = model(x)
-    ce = sum(Flux.binarycrossentropy.(y, ŷ; ϵ=eps(Float32)))
-    if print_loss
-        print("Loss: $ce")
-    end
-    return ce
-end
-
-function make_minibatch(X, Y, idxs)
-    noverlap = 256
-    nfft = 512
-    sz = div(size(X, 1), noverlap) - 1
+function make_batch(data, labels, idxs, minibatch_size, cat_mapping; divider = 1, n = 512, noverlap = div(n, 2))
+    nfft = nextfastfft(n)
+    sz = div(div(size(data, 1), noverlap), divider) - 1
     X_batch = Array{Float32}(undef, div(nfft, 2) + 1, sz, 1, length(idxs))
+    Y_batch = Array{UInt8}(undef, length(idxs))
     win = hanning(nfft)
+    data_ind = 1:div(size(data, 1), divider)
     for i in 1:length(idxs)
-        st = abs.(stft(Float32.(X[:,idxs[i]][:]./typemax(Int16)), nfft, noverlap, window=win))
-        X_batch[:, :, :, i] = epsnormalise(20*log10.(max.(st, eps(Float32))))
+        st = abs.(stft(Float32.(data[data_ind,idxs[i]][:]./typemax(Int16)), n, noverlap, window=win, nfft=nfft))
+        X_batch[:, :, 1, i] = epsnormalise(20*log10.(max.(st, eps(Float32))))
+        Y_batch[i] = cat_mapping[labels[idxs[i]][1]]
     end
-    Y_batch = onehotbatch(Y[idxs[1]:idxs[end]], 0:3)
-    return (X_batch, Y_batch)
+    return minibatch(convert(KnetArray, X_batch), Y_batch, minibatch_size)
 end
 
-model = Chain(
-    Conv((3, 3), 1=>16, relu),
-    MaxPool((2,2)),
-
-    Conv((3, 3), 16=>16, relu),
-    MaxPool((2,2)),
-
-    Conv((3, 3), 16=>16, relu),
-    MaxPool((2,2)),
-
-    Conv((3, 3), 16=>16, relu),
-    MaxPool((2,2)),
-
-    x -> reshape(x, :, size(x, 4)),
-
-    Dense(71904, 256),
-    x -> relu.(x),
-    Dropout(0.2),
-
-    Dense(256, 256),
-    x -> relu.(x),
-    Dropout(0.2),
-
-    Dense(256, 4),
-    softmax
-)
-
-accuracy(x, y) = mean(onecold(model(x)) .== onecold(y))
-function mbfn(fn, set)
-    m = 0
-    for i = 1:length(set)
-        m += fn(set[i]...)
+function confusion(model, data, size)
+    conf = zeros(Float32, size, size)
+    for mbatch in data
+        pred = convert(Array, model(mbatch[1]))
+        for ind = 1:length(mbatch[2])
+            conf[findmax(pred[ind, :])[2], mbatch[2][ind]] += 1
+        end
     end
-    return m / length(set)
+    return conf ./ length(data)
 end
 
-function make_batch(data, labels, idxs)
-    idxs = idxs[1]:idxs[end]
-    data = data[:,idxs]
-    labels = labels[idxs]
-    return [make_minibatch(data, labels, i) for i in minibatch_idxs]
-end
+cat_mapping =
+Dict{Int8, UInt8}(
+                  0  => 1, # WAV
+                  11 => 2, # MP3 320
+                  12 => 3, # MP3 192
+                  13 => 4, # MP3 128
+                  21 => 5, # AAC 320
+                  22 => 6, # AAC 192
+                  23 => 7, # AAC 128
+                  31 => 8, # OGG 320
+                  32 => 9, # OGG 192
+                  33 => 10 # OGG 128
+                 ) 
 
-minibatch_size = 2
-batch_size = 16
-train_size = 6144
+no_categories = 10
+
+minibatch_size = 4
+train_batch_size = 128
+test_batch_size = 512
+train_size = 4096
 test_size = 2048
 
-minibatch_idxs = partition(1:batch_size, minibatch_size)
-train_batch_idxs = partition(1:train_size, batch_size)
-test_batch_idxs = partition((train_size+1):(train_size+test_size), batch_size)
+train_batch_idxs = collect(partition(1:train_size, train_batch_size))
+test_batch_idxs = partition((train_size+1):(train_size+test_size), test_batch_size)
 
-opt = ADAM(0.000001)
+struct Conv; w; b; f; p; end
+(c::Conv)(x) = c.f.(pool(conv4(c.w, dropout(x,c.p)) .+ c.b))
+Conv(w1::Int,w2::Int,cx::Int,cy::Int,f=relu;pdrop=0) = Conv(param(w1,w2,cx,cy), param0(1,1,cy,1), f, pdrop)
 
-batches = length(train_batch_idxs)
-minibatches = length(minibatch_idxs)
+struct Dense; w; b; f; p; end
+(d::Dense)(x) = d.f.(d.w * mat(dropout(x,d.p)) .+ d.b) # mat reshapes 4-D tensor to 2-D matrix so we can use matmul
+Dense(i::Int,o::Int,f=relu;pdrop=0) = Dense(param(o,i), param0(o), f, pdrop)
 
-best_acc = 0.0
-last_improvement = 0
+struct Chain; layers; Chain(layers...) = new(layers); end
+(c::Chain)(x) = (for l in c.layers; x = l(x); end; x)
+(c::Chain)(x,y) = nll(c(x),y)
+(c::Chain)(d::Data) = mean(c(x,y) for (x,y) in d)
 
-@info("Starting training loop")
-for epoch_idx in 1:100
-    global best_acc, last_improvement
-    ps = params(model)
-    b_it = 0
-    test_acc = 0
-    train_acc = 0
-    test_accs = Array{Float32, 1}()
-    train_accs = Array{Float32, 1}()
+function train!(epochs, load=true, train=true, test=true)
+    gc()
+    Knet.gc()
 
-    h5open("./dataset.h5", "r") do file
+    model, test_confs = if load
+        lf = Knet.load("model.jld2")
+        lf["model"], lf["test_confs"]
+    else
+        Chain(Conv(3, 3, 1, 16),
+              Conv(3, 3, 16, 16),
+              Conv(3, 3, 16, 16),
+              Dense(102240, 256, pdrop=0.3),
+              Dense(256, 256, pdrop=0.2),
+              Dense(256, no_categories, identity, pdrop=0.2)),
+        Array{Array{Float32, 2}, 1}()
+    end
+    best_acc = length(test_confs) > 0 ? mean(diag(test_confs[end])) : 0
+
+    h5open("./dataset.h5") do file
         data = file["data"]
         labels = file["labels"]
 
-        for data_ind in train_batch_idxs
-            mb_it = 0
-            b_it += 1
-            let train_set = make_batch(data, labels, data_ind)
-                Flux.train!(loss, ps, train_set, opt, cb = () -> print(" Batch: $(b_it)/$batches Minibatch: $(mb_it+=1)/$minibatches                             \r"))
-                train_acc += mbfn(accuracy, train_set)
+        for epoch in 1:epochs
+            @info "Epoch: $epoch"
+            shuffle!(train_batch_idxs)
+            if train
+                for idx in progress(train_batch_idxs)
+                    let dtrn = make_batch(data, labels, idx, minibatch_size, cat_mapping)
+                        for a in adam(model, dtrn); end
+                    end
+                    gc()
+                end
             end
-            gc()
-        end
-        train_acc /= length(train_batch_idxs)
 
-        b_it = 0
-        for data_ind in test_batch_idxs
-            let test_set = make_batch(data, labels, data_ind)
-                print("Batch: $(b_it+=1)/$(length(test_batch_idxs))                              \r")
-                test_acc += mbfn(accuracy, test_set)
+            if test
+                test_conf = zeros(no_categories, no_categories)
+                for idx in progress(test_batch_idxs)
+                    let dtst = make_batch(data, labels, idx, minibatch_size, cat_mapping)
+                        test_conf .+= confusion(model, dtst, no_categories)
+                    end
+                    gc()
+                end
+                test_conf ./= length(test_batch_idxs)
+
+                push!(test_confs, test_conf)
+
+                acc = mean(diag(test_conf))
+
+                println("Confusion Matrix:")
+                display(test_conf)
+                println("Accuracy: $acc")
+            else
+                acc = best_acc + 1
             end
-            gc()
+            if acc > best_acc
+                best_acc = acc
+                Knet.save("model.jld2", "model", model, "test_confs", test_confs)
+            end
         end
-        test_acc /= length(test_batch_idxs)
     end
 
-
-    @info(@sprintf("[%d]: Accuracy: %.4f Test accuracy: %.4f", epoch_idx, train_acc, test_acc))
-    push!(train_accs, train_acc)
-    push!(test_accs, test_acc)
-    BSON.@save "iola_hist.bson" train_accs test_accs
-
-    # If our accuracy is good enough, quit out.
-    if test_acc >= 0.999
-        @info(" -> Early-exiting: We reached our target accuracy of 99.9%")
-        break
-    end
-
-    # If this is the best accuracy we've seen so far, save the model out
-    if test_acc > best_acc
-        @info(" -> New best accuracy! Saving model out to iola_conv.bson")
-        BSON.@save "mnist_conv.bson" model epoch_idx test_acc
-        best_acc = test_acc
-        last_improvement = epoch_idx
-    end
-
-    # If we haven't seen improvement in 5 epochs, drop our learning rate:
-    if epoch_idx - last_improvement >= 5 && opt.eta > 1e-9
-        opt.eta /= 10.0
-        @warn(" -> Haven't improved in a while, dropping learning rate to $(opt.eta)!")
-
-        # After dropping learning rate, give it a few epochs to improve
-        last_improvement = epoch_idx
-    end
-
-    if epoch_idx - last_improvement >= 10
-        @warn(" -> We're calling this converged.")
-        break
-    end
+    return model, test_confs
 end
+
