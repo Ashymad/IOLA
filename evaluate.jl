73d0cfc90c9992dd5152168d03c82f3321f0a6d0
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2019-08-15 22:56:12 +0200

    Auto-find number of points.

--- evaluate.jl
+++ evaluate.jl
@@ -1,12 +1,9 @@
 using IOLA
 using IOLA.Utils
-using MDCT
 using HDF5
-using Statistics
 
-fs = 44100
-seg_len = 1
-res_size = 4096
+max_points = 100
+res_size = 2048
 codecs_num = length(instances(Codec.CodecType))
 
 parameters = Dict{Codec.CodecType, Tuple{Function,Int64,Int64}}()
@@ -15,28 +12,49 @@ function to_float64(arr::AbstractArray{T}) where T<:Integer
     return convert.(Float64, arr)./typemax(T)
 end
 
-for codec in instances(Codec.CodecType)
-    par = Codec.getparams(codec)
-    len = ceil(Int64, seg_len*fs/par.length)*par.length
-    push!(parameters, codec => (Codec.gettransform(par), len, par.hop))
-end
+
+
+
 
 h5open("dataset.h5") do dataset
     data = dataset["data"]
+    siglen = size(data,1)
+    po = max_points + 1
+    @label loop1
+    po -= 1
+    prev_rpo = 0
+    for codec in instances(Codec.CodecType)
+        par = Codec.getparams(codec)
+        len = (siglen-par.hop)/po
+        len = round(len/par.length)*par.length
+        rpo = div(siglen-par.hop,len)
+        prev_rpo = prev_rpo == 0 ? rpo : prev_rpo
+        if  prev_rpo != rpo
+            @goto loop1
+        end
+    end
+    reslen = Int(prev_rpo)
+    println("Number of points: $reslen")
+
+    for codec in instances(Codec.CodecType)
+        par = Codec.getparams(codec)
+        len = (siglen-par.hop)/po
+        len = round(len/par.length)*par.length
+        push!(parameters, codec => (Codec.gettransform(par), len, par.hop))
+    end
     h5open("results.h5", "w") do results
-        KD_size = Int(size(data,1)/fs - 1)
         C = d_create(results, "C", datatype(Float64),
                      dataspace(codecs_num,res_size),
                      "chunk", (codecs_num,1))
         KD = d_create(results, "KD", datatype(Float64),
-                      dataspace(KD_size,2,codecs_num,res_size),
-                      "chunk", (KD_size,2,codecs_num,1))
+                      dataspace(reslen,2,codecs_num,res_size),
+                      "chunk", (reslen,2,codecs_num,1))
         p = d_create(results, "p", datatype(Int64),
                      dataspace(codecs_num,res_size),
                      "chunk", (codecs_num,1))
 
         C_tmp = zeros(Float64,codecs_num)
-        KD_tmp = zeros(Float64,KD_size,2,codecs_num)
+        KD_tmp = zeros(Float64,reslen,2,codecs_num)
         p_tmp = zeros(Int64,codecs_num)
         
         for data_i in 1:res_size
